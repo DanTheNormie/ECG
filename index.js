9f63e816f0b74ba047aa9666e1557e997869b6a7
CmdDaniel <cmd.princedaniel@gmail.com>
2023-08-23 13:26:20 +0530

    self-discipline.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const axios = require('axios')
 const PORT = process.env.PORT || 2000
 
-app.get('/live',(res)=>{res.sendStatus(200)})
+app.get('/live',(req,res)=>{res.sendStatus(200)})
 
 console.log('Checking CST for heartbeat...');
 axios.get('https://cst-lcg0.onrender.com/live')
@@ -21,6 +21,9 @@ setInterval(()=>{
             console.log(err);
             console.log('CST heartbeat not detected...')
         })
+
+    axios.get('https://dannys-ecg.onrender.com/live')
+        .then((()=>console.log('🥲👍 I\'m awake ...')))
 
 },1000*60*14)
 
