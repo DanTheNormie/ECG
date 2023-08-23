ad7bfcc9e93bd9a8fc51d626692efd3d220b709e
CmdDaniel <cmd.princedaniel@gmail.com>
2023-08-23 13:05:03 +0530

    changed fetch to axios

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 const express = require('express')
 const app = express()
+const axios = require('axios')
 const PORT = process.env.PORT || 2000
 
 app.get('/live',(res)=>{res.sendStatus(200)})
 
 console.log('Checking CST for heartbeat...');
-    fetch('https://cst-lcg0.onrender.com/live')
-        .then(((data)=>console.log('CST heartbeat detected...')))
+axios.get('https://cst-lcg0.onrender.com/live')
+    .then((()=>console.log('CST heartbeat detected...')))
+    .catch((err)=>{
+        console.log(err);
+        console.log('CST heartbeat not detected...')
+    })
+
+setInterval(()=>{
+    console.log('Checking CST for heartbeat...');
+    axios.get('https://cst-lcg0.onrender.com/live')
+        .then((()=>console.log('CST heartbeat detected...')))
         .catch((err)=>{
             console.log(err);
-            console.log('CST heartbeat not detected...')
-        })
-
-
-setInterval(async ()=>{
-    console.log('Checking CST for heartbeat...');
-    fetch('https://cst-lcg0.onrender.com/live')
-        .then(((data)=>console.log('CST heartbeat detected...')))
-        .catch((err)=>{
             console.log('CST heartbeat not detected...')
         })
 
